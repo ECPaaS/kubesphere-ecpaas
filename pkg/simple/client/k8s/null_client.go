62c741f2a0f1c0c6541aca57d778a81e439b8754
Macauley Cheng <macauley_cheng@accton.com>
2025-05-02 11:10:35 +0800

    Revert "Integrate OVN DSCP into DSCP K8s controller (#77)" (#78)
    This reverts commit e0a66da31005922c06aa4c9cc7d20dbbb29f05b3.

--- pkg/simple/client/k8s/null_client.go
+++ pkg/simple/client/k8s/null_client.go
@@ -22,7 +22,6 @@ import (
 	istio "istio.io/client-go/pkg/clientset/versioned"
 	apiextensionsclient "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset"
 	"k8s.io/client-go/discovery"
-	"k8s.io/client-go/dynamic"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 
@@ -37,10 +36,6 @@ func NewNullClient() Client {
 }
 
 func (n nullClient) Kubernetes() kubernetes.Interface {
-	return nil
-}
-
-func (n nullClient) Dynamic() dynamic.Interface {
 	return nil
 }
 
